1b189a5ddf2d4a50780766df5382d1194a4d66b8
Vishnu123454321 <132432209+Vishnu123454321@users.noreply.github.com>
2023-08-09 09:02:41 +0530

    square root.cpp

---
+++ square root.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+#include <math.h> 
+int main()
+{
+	float x;
+	printf("enter the value x :");
+	scanf("%f",&x);
+	printf("square root of %6.2f is %6.2f",x,sqrt(x));
+	getch();
+}
